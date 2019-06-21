035aebb6c1abdc7f886a736672a8ed39f5c6abee
Petr Kočí <petr.koci@samizdat.cz>
2019-06-21 10:51:01 +0200

    get data

---
+++ js/getData.js
@@ -0,0 +1,13 @@
+const https = require('https');
+const fs = require('fs');
+const d3 = require('d3');
+const roky = [2015,2016,2017,2018,2019];
+
+roky.forEach((rok, i) => {
+  const fileURL = 'https://www.mdcr.cz/MDCR/media/otevrenadata/faktury/' + rok + '/faktury_md_' + rok + '.csv';
+  const file = fs.createWriteStream('data/faktury_md_' + rok + '.csv');
+  const request = https.get(fileURL, response => response.pipe(file));
+  console.log(fileURL);
+});
+
+console.log(d3);
