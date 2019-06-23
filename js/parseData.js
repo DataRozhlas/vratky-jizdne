7d50f82c1ed023d6b085ed1706cbecbd60606c2d
Petr Kočí <petr.koci@samizdat.cz>
2019-06-23 23:00:13 +0200

    lint + hc klíče

--- js/parseData.js
+++ js/parseData.js
@@ -1,37 +1,32 @@
 const fs = require('fs');
 const d3 = require('d3');
-const roky = [2015,2016,2017,2018,2019];
+
+const roky = [2015, 2016, 2017, 2018, 2019];
 let vysledek = [];
 let itemsProcessed = 0;
 
 const parseData = (rok, callback) => {
-    fs.readFileSync('data/faktury_md_' + rok + '.csv', (err, data) => {
-        callback(d3.csvParse(data.toString('utf-8')));        
-     });
-}
+  fs.readFile(`data/faktury_md_${rok}.csv`, (err, data) => {
+    callback(d3.csvParse(data.toString('utf-8')));
+  });
+};
 
 roky.forEach((rok, index, array) => {
   parseData(rok, (data) => {
     vysledek = vysledek.concat(data);
-    itemsProcessed ++;
+    itemsProcessed += 1;
     if (itemsProcessed === array.length) {
-        let kompenzace = vysledek.filter(faktura => {
-            return faktura.UCELPLATBY.toUpperCase().includes('KOMPENZACE');
-        });
-        kompenzace = kompenzace.map(faktura => {
-            return {
-                c : faktura.CISLOFA,
-                ic: faktura.ICDOD,
-                dod: faktura.DODAVATEL,
-                u: faktura.UCELPLATBY,
-                kc: faktura.CASTKA,
-                dat: new Date(faktura.DATVYST),
-            }
-        });
-        kompenzace = kompenzace.sort((a, b) => {
-            return a.dat - b.dat;
-        });
-        fs.writeFileSync('data/data.json', JSON.stringify(kompenzace));
+      let kompenzace = vysledek.filter(faktura => faktura.UCELPLATBY.toUpperCase().includes('KOMPENZACE'));
+      kompenzace = kompenzace.map(faktura => ({
+        c: faktura.CISLOFA,
+        i: faktura.ICDOD,
+        d: faktura.DODAVATEL,
+        u: faktura.UCELPLATBY,
+        x: faktura.CASTKA,
+        y: new Date(faktura.DATVYST),
+      }));
+      kompenzace = kompenzace.sort((a, b) => a.dat - b.dat);
+      fs.writeFileSync('data/data.json', JSON.stringify(kompenzace));
     }
-  });    
+  });
 });
--- js/script.js
+++ js/script.js
@@ -1,7 +1,46 @@
-﻿import "./byeie"; // loučíme se s IE
+﻿import './byeie'; // loučíme se s IE
 
-fetch("https://data.irozhlas.cz/vratky-jizdne/js/data/data.json")
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
+const rendruj = (data) => {
+  const unikatniIC = [...new Set(data.map(x => x.ic))];
+  // unikatniIC.forEach((ic) => {
+  //   console.log(data.filter(zaznam => ic === zaznam.ic));
+  // });
+  console.log(data);
+  Highcharts.stockChart('graf', {
+    chart: {
+      alignTicks: false,
+    },
+    rangeSelector: {
+      selected: 1,
+    },
+    title: {
+      text: 'AAPL Stock Volume',
+    },
+    series: [{
+      turboThreshold: 4000,
+      type: 'column',
+      name: 'AAPL Stock Volume',
+      data: data,
+      dataGrouping: {
+        units: [[
+          'week', // unit name
+          [1]], [
+          'month',
+          [1, 2, 3, 4, 6]]],
+      },
+    }],
   });
+};
+
+fetch('https://data.irozhlas.cz/vratky-jizdne/js/data/data.json')
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Error getting the stuff');
+    }
+    return response;
+  })
+  .then(result => result.json())
+  .then((data) => {
+    rendruj(data);
+  })
+  .catch(err => console.log(err));
